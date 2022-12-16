160557eb92cef2da2477f5cbbedd0cd7293994cd
Rakib Khan <khanrakib218@gmail.com>
2022-12-16 11:03:59 +0600

    List of even numbers

--- src/ListEvenNumbers.java
+++ src/ListEvenNumbers.java
@@ -1,10 +1,9 @@
-//List of even numbers
+//List of even numbers from 1 to 50
 public class ListEvenNumbers {
     public static void main(String[] args) {
 //define limit
         int limit = 50;
-        System.out.println("Printing Even numbers between 1 and " +
-                limit);
+        System.out.println("Printing Even numbers between 1 and " + limit);
         for(int i=1; i <= limit; i++){
 // if the number is divisible by 2 then it is even
             if( i % 2 == 0){

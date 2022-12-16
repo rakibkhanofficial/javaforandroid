1cb10d4a004ebbbb0eff5839510dcfb98cc05a73
Rakib Khan <khanrakib218@gmail.com>
2022-12-16 12:12:33 +0600

    List of Even numbers between 1 and 100

--- src/ListOfEvenNumbers.java
+++ src/ListOfEvenNumbers.java
@@ -1,14 +1,15 @@
-//List of even numbers from 1 to 50
-public class ListEvenNumbers {
+//List of even numbers from 1 to 100
+public class ListOfEvenNumbers {
     public static void main(String[] args) {
 //define limit
-        int limit = 50;
-        System.out.println("Printing Even numbers between 1 and " + limit);
+        int limit = 100;
+        System.out.println("Even numbers between 1 and " + limit);
         for(int i=1; i <= limit; i++){
 // if the number is divisible by 2 then it is even
             if( i % 2 == 0){
                 System.out.print(i + " ");
             }
         }
+
     }
 }
